9792a5af5feeba86091a34462ae4aa2d1ca7b812
Marcus Maciel <mendesxx.ti@hotmail.com>
2019-12-17 11:55:54 -0300

    Ajuste

--- src/main/kotlin/br/com/swapi/swapi/SwapiApplication.kt
+++ src/main/kotlin/br/com/swapi/swapi/SwapiApplication.kt
@@ -5,7 +5,6 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
-// @ComponentScan(value = ["br.com.swapi.swapi.service", "br.com.swapi.swapi.lib", "br.com.swapi.swapi.repository"])
 class SwapiApplication
 
 fun main(args: Array<String>) {
